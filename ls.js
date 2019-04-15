4e648f06af1036a5047b8faebea967f5583ef2f0
Stanislav Lohvinov <stanislav.lohvinov@decimadigital.com>
2019-04-15 11:22:16 +0300

    full rebuild

--- ls.js
+++ ls.js
@@ -1,40 +1,18 @@
-// TODO: make scoped function!
-
-// const LS = localStorage;
-
-// const parseStr = JSON.parse;
-
-// const stringify = JSON.stringify;
-
-// const saveToLS = (k, v) => {
-//   LS.setItem(k, stringify(v));
-// };
-
-// const rmFromLS = k => {
-//   LS.removeItem(k);
-// };
-
-// const getFromLS = k => {
-//   try {
-//     return parseStr(LS.getItem(k));
-//   } catch (e) {
-//     rmFromLS(k);
-//   }
-// };
-
 const LS = (() => {
   const LS = localStorage;
   const parseStr = JSON.parse;
   const stringify = JSON.stringify;
 
   return {
-    save: (k, v) => LS.setItem(k, stringify(v)),
+    set: (k, v) => LS.setItem(k, stringify(v)),
     remove: k => LS.removeItem(k),
-    get(k) {
-      return parseStr(LS.getItem(k)) || this.remove(k);
+    get: k => parseStr(LS.getItem(k)),
+    getAll: () => {
+      const result = {};
+      Object.keys(LS).forEach(key => (result[key] = parseStr(LS.getItem(key))));
+      return result;
     },
-    len: () => LS.length,
-    getAll: () => LS,
+    getLen: () => LS.length,
     clear: () => LS.clear(),
   };
 })();
--- script.js
+++ script.js
@@ -1,65 +1,108 @@
 const doc = document;
 const byId = id => doc.getElementById(id);
 
+const TDL = new TodoList();
+
 doc.addEventListener('DOMContentLoaded', function() {
   const form = byId('form');
 
-  getTasksFromLS();
+  renderList();
 
-  form.addEventListener('submit', ev => {
+  byId('form-submit').addEventListener('click', ev => {
     ev.preventDefault();
-    createTask();
-    refreshTaskBoard();
+    TDL.addTask(createTask());
+    renderList();
+    formReset('form');
   });
+
+  byId('sort-by-name').addEventListener('click', () => liveSortBy('name'));
+  byId('sort-by-date').addEventListener('click', () => liveSortBy('date'));
 });
 
 /* functions */
-function createTask() {
-  const taskName = form.elements[0].value;
-  const taskDate = form.elements[1].value;
-  const taskDescr = form.elements[2].value;
-  const task = new Task(taskName, taskDate, taskDescr);
+function renderList() {
+  byId('list').innerHTML = '';
 
-  LS.save(task.id, task);
-}
-
-function getTasksFromLS() {
-  Object.values(LS.getAll()).forEach(taskData => {
+  TDL.getList().forEach(taskData => {
     const task = document.createElement('li');
-    task.innerHTML = taskTemplate(JSON.parse(taskData));
+    task.innerHTML = taskTemplate(taskData);
     byId('list').appendChild(task);
   });
 }
 
-function refreshTaskBoard() {
-  byId('list').innerHTML = '';
-  getTasksFromLS();
+function formReset(id) {
+  byId(id).reset();
+}
+
+function liveSortBy(param) {
+  TDL.sortBy(param);
+  renderList();
 }
 
 function liveTaskDelete(id) {
-  LS.remove(id);
-  refreshTaskBoard();
+  TDL.remove(id);
+  renderList();
 }
 
 function liveStatusChange(id, statusCode) {
-  let task = LS.get(id);
-  if (task.statusCode === statusCode) return;
-  task.statusCode = statusCode;
-  LS.save(id, task);
-  refreshTaskBoard();
+  TDL.statusChange(id, statusCode);
+  renderList();
 }
 
-function liveEditTask(id) {
-  showEditForm();
-  let task = LS.get(id);
+function createTask() {
+  const taskName = form.elements[0].value;
+  const dueDate = form.elements[1].value;
+  const taskDescr = form.elements[2].value;
+  return new Task(taskName, dueDate, taskDescr);
 }
 
-function showEditForm() {
+function showEditForm(id) {
   doc.body.classList.add('editing-mode');
-  // TODO: add event listener to edit form background (for close and cancel changes)
+  const task = TDL.getTask(id);
+  // const popupBg = byId('popup-bg');
+  const editForm = byId('editing-form');
+  const taskNameInp = editForm.elements[0];
+  const taskDueInp = editForm.elements[1];
+  const taskDescrInp = editForm.elements[2];
+  const cancelBtn = byId('edit-cancel');
+
+  taskNameInp.value = task.name;
+  taskDueInp.value = task.dueDate;
+  taskDescrInp.value = task.descr;
+
+  cancelBtn.addEventListener(
+    'click',
+    () => {
+      console.log('>>>');
+      doc.body.classList.remove('editing-mode');
+    },
+    { once: true },
+  );
+
+  editForm.addEventListener('submit', ev => {
+    ev.preventDefault();
+    const editedTask = {
+      ...task,
+      name: taskNameInp.value,
+      dueDate: taskDueInp.value,
+      descr: taskDescrInp.value,
+    };
+    TDL.update(editedTask);
+    doc.body.classList.remove('editing-mode');
+    renderList();
+  });
 }
 
-function taskTemplate({ statusCode = 0, id, name, date, descr, wasEdited }) {
+// TODO: dueDate
+function taskTemplate({
+  statusCode = 0,
+  id,
+  name,
+  dueDate,
+  created,
+  descr,
+  wasEdited,
+}) {
   const status = (() => {
     switch (statusCode) {
       case 0:
@@ -71,15 +114,18 @@ function taskTemplate({ statusCode = 0, id, name, date, descr, wasEdited }) {
     }
   })();
 
-  const editBtnTemplate = `<button onclick="liveEditTask(${id})" id="btn-edit-task" class="btn btn_primary task__btn task__btn-edit"></button>`;
+  const editBtnTemplate = `<button onclick="showEditForm(${id})" id="btn-edit-task" class="btn btn_primary task__btn task__btn-edit"></button>`;
   const editedMark = `<span class="task__was-edited">edited</span>`;
 
   return `
-  <li class="task task_${status}">
+  <div class="task task_${status}">
     <header class="task__header">
       <div class="task__info">
         <span class="task__name">${name}</span>
-        <span class="task__date">${date}</span>
+        <span class="task__created">Created: ${created}</span>
+      </div>
+      ${wasEdited ? editedMark : ''}
+      <div class="task__controls">
         <div class="task__status">
           <span class="status__current">${status}</span>
           <ul class="status__dropdown">
@@ -88,26 +134,85 @@ function taskTemplate({ statusCode = 0, id, name, date, descr, wasEdited }) {
             <li onclick="liveStatusChange(${id},${2})" class="status__option">done</li>
           </ul>
         </div>
-      </div>
-      ${wasEdited ? editedMark : ''}
-      <div class="task__controls">
-        ${statusCode < 2 ? editBtnTemplate : ''}
-        <button id="btn-delete-task" onclick="liveTaskDelete(${id})" class="btn btn_danger task__btn task__btn-delete"></button>
+        <div class="task__actions">
+          ${statusCode < 2 ? editBtnTemplate : ''}
+          <button id="btn-delete-task" onclick="liveTaskDelete(${id})" class="btn btn_danger task__btn task__btn-delete"></button>
+        </div>
       </div>
     </header>
     <div class="task__content">
       ${descr}
     </div>
-  </li>
+  </div>
   `;
 }
 
 /* constructors */
+function TodoList() {
+  const LIST = Object.values(LS.getAll());
+  const saveList = () => {
+    LS.clear();
+    LIST.map(item => LS.set(item.id, item));
+  };
+  const taskMap = () => LIST.map(item => item.id);
+
+  const getTask = id => {
+    const index = taskMap().indexOf(id);
+    return LIST[index];
+  };
+
+  return {
+    getTask,
+    addTask: item => {
+      LIST.push(item);
+      saveList();
+    },
+    getList: () => LIST,
+    update: elm => {
+      const index = taskMap().indexOf(elm.id);
+      LIST.splice(index, 1, elm);
+      saveList();
+    },
+    remove: id => {
+      const index = taskMap().indexOf(id);
+      LIST.splice(index, 1);
+      saveList();
+    },
+    sortBy: param => {
+      switch (param) {
+        case 'name':
+          LIST.sort(compareByName);
+          break;
+        case 'date':
+          LIST.sort(compareByDate);
+          break;
+      }
+      saveList();
+      function compareByName(x, y) {
+        x = x.name.toLowerCase()[0];
+        y = y.name.toLowerCase()[0];
+        return x > y ? 1 : -1;
+      }
+      function compareByDate(x, y) {
+        return x.id - y.id;
+      }
+    },
+    statusChange: (id, statusCode) => {
+      const task = getTask(id);
+      if (task.statusCode === statusCode) return;
+      task.statusCode = statusCode;
+      saveList();
+    },
+  };
+}
+
 class Task {
-  constructor(name, date, descr) {
-    // this.created = new Date(date);
-    this.created = Date.now();
-    this.date = date;
+  constructor(name, dueDate, descr) {
+    this.dueDate = dueDate;
+    this.created = (() => {
+      const date = new Date();
+      return date.toDateString();
+    })();
     this.name = name;
     this.descr = descr;
     this.statusCode = 0;
